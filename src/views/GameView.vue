18cd66e8d2cb52cdc86e6eea24765f30b43e8641
mpittas <webpittas@gmail.com>
2024-09-06 18:45:34 +0300

    Update error fix 3

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,34 +1,35 @@
 <template>
   <div class="min-h-screen bg-red-50">
     <div class="container mx-auto">
-      <div v-if="game" class="game-view">
-        <h2 class="text-2xl mb-8 pb-4 border-b border-black">{{ game.title }}</h2>
-        <component :is="currentGameComponent" />
-      </div>
+      <template v-if="gameData">
+        <div class="game-view">
+          <h2 class="text-2xl mb-8 pb-4 border-b border-black">{{ gameData.title }}</h2>
+          <component :is="currentGameComponent" />
+        </div>
+      </template>
       <div v-else>Game not found</div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref, defineAsyncComponent } from 'vue'
+import { computed, ref, defineAsyncComponent, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { GAMES } from '@/constants/games'
 import type { Game } from '@/constants/games'
 
 const route = useRoute()
 const gameId = computed(() => route.params.id as string)
-
-const game = ref<Game | undefined>(GAMES.find((g) => g.id === gameId.value))
+const gameData = ref<Game | null>(null)
 
 const currentGameComponent = computed(() => {
-  return game.value
-    ? defineAsyncComponent(
-        () =>
-          import(
-            `@/components/games/${game.value.id.charAt(0).toUpperCase() + game.value.id.slice(1)}Game.vue`
-          )
-      )
-    : null
+  if (!gameData.value) return null
+  const componentName = gameData.value.id.charAt(0).toUpperCase() + gameData.value.id.slice(1)
+  return defineAsyncComponent(() => import(`@/components/games/${componentName}Game.vue`))
+})
+
+onMounted(() => {
+  const foundGame = GAMES.find((g) => g.id === gameId.value)
+  gameData.value = foundGame || null
 })
 </script>

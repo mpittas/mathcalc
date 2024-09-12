d2186cea37559aeea71806dfd89f2f31e5aa447e
mpittas <webpittas@gmail.com>
2024-09-12 16:59:16 +0300

    Test error fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx()
-    // vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx()],
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer]
@@ -22,14 +17,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: fileURLToPath(new URL('./index.html', import.meta.url)),
-        _redirects: fileURLToPath(new URL('./public/_redirects', import.meta.url))
-      }
     }
   }
 })
